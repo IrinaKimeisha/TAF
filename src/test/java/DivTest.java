481cd151003039aa29d8c36db7f8f15ab94ad9c3
IrinaKimeisha <irinkakim777@gmail.com>
2022-10-03 15:38:20 +0300

    дз в процессе.версия 1.1

--- src/test/java/DivTest.java
+++ src/test/java/DivTest.java
@@ -1,39 +1,62 @@
+import data.StaticProvider;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DivTest extends BaseTest {
     @Test
-    public void divInt() {
+    public void divIntTest() {
         Assert.assertEquals(calculator.div(10, 5), 2, "Неверное деление...");
     }
 
     @Test
-    public void divDouble() {
+    public void divDoubleTest() {
         Assert.assertEquals(calculator.div(10.0, 5.0), 2.0, "Неверное деление...");
     }
 
     @Test(enabled = false)
-    public void divInt1() {
-        Assert.assertEquals(calculator.div(10, 2), 1, "Неверное деление...");
+    public void divIntTest1() {
+        Assert.assertEquals(calculator.div(10, 2), 5, "Неверное деление...");
     }
 
     @Test(enabled = false)
-    public void divDouble1() {
-        Assert.assertEquals(calculator.div(10.0, 2.0), 1.0, "Неверное деление...");
-    }
-    @Test(timeOut = 1000)
-    public void divIntTimeOut() throws InterruptedException {
-        Thread.sleep(900);
-        Assert.assertEquals(calculator.div(10, 2), 5, "Неверное деление...");
-    }
-    @Test(timeOut = 1000)
-    public void divDoubleTimeOut() throws InterruptedException {
-        Thread.sleep(500);
+    public void divDoubleTest1() {
         Assert.assertEquals(calculator.div(10.0, 2.0), 5.0, "Неверное деление...");
     }
 
 
+    @Test(timeOut = 1000)
+    public void divIntTimeOutTest() throws InterruptedException {
+        Thread.sleep(900);
+        Assert.assertEquals(calculator.div(10, 2), 5, "Неверное деление...");
+    }
 
+    @Test(timeOut = 1000)
+    public void divDoubleTimeOutTest() throws InterruptedException {
+        Thread.sleep(500);
+        Assert.assertEquals(calculator.div(10.0, 2.0), 5.0, "Неверное деление...");
+    }
+
+    @Test(invocationCount = 5, invocationTimeOut = 1000, threadPoolSize = 5)
+    public void divIntInvocationCountTest() throws InterruptedException {
+        Thread.sleep(500);
+        Assert.assertEquals(calculator.div(10, 2), 5, "Неверная деление...");
+    }
+
+    @Test(invocationCount = 2, invocationTimeOut = 1000, threadPoolSize = 2)
+    public void divDoubleInvocationCountTest() throws InterruptedException {
+        Thread.sleep(500);
+        Assert.assertEquals(calculator.div(10.0, 2.0), 5.0, "Неверная деление...");
+    }
+
+    @Test(dataProvider = "dataForIntDiv", dataProviderClass = StaticProvider.class)
+    public void testDataProvider(int a, int b, int expectedResult) {
+        Assert.assertEquals(calculator.div(a, b), expectedResult, "Неверная деление...");
+    }
+
+    @Test(dataProvider = "dataForDoubleDiv", dataProviderClass = StaticProvider.class)
+    public void testDataProvider(double a, double b, double expectedResult) {
+        Assert.assertEquals(calculator.div(a, b), expectedResult, "Неверная деление...");
+    }
 
 
 }
--- src/test/java/data/StaticProvider.java
+++ src/test/java/data/StaticProvider.java
@@ -4,12 +4,29 @@ import org.testng.annotations.DataProvider;
 
 public class StaticProvider {
 
-    @DataProvider (name = "dataForSum")
+   /* @DataProvider (name = "dataForSum")
     public static Object[][] dataForSumTest() {
         return new Object[][] {
                 {-2, -3, -5},
                 {0, 0, 0},
                 {2, 3, 5}
         };
+    }*/
+    @DataProvider (name = "dataForIntDiv")
+    public static Object[][] dataForIntDivTest() {
+        return new Object[][] {
+                {-6, -3, 2},
+                {0, 1, 0},
+                {8, 2, 4}
+        };
     }
+    @DataProvider (name = "dataForDoubleDiv")
+    public static Object[][] dataForDoubleDivTest() {
+        return new Object[][] {
+                {-6.0, -3.0, 2.0},
+                {0, 1.0, 0},
+                {8.0, 2.0, 4.0}
+        };
+    }
+
 }
