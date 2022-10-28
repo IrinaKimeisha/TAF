4a7e70492e5dd59068c472378b15643d1f1939c1
IrinaKimeisha <irinkakim777@gmail.com>
2022-10-28 22:57:15 +0300

    добавила Listener

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -4,10 +4,8 @@ import configuration.ReadProperties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
 import services.BrowsersService;
 import steps.LoginStep;
-import utils.Listener;
 
 //@Listeners(Listener.class)
 public class BaseTest {
