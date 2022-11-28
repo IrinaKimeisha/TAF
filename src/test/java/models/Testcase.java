a217091e1c01863482954147389197db0fafe43c
IrinaKimeisha <irinkakim777@gmail.com>
2022-11-28 19:07:07 +0300

    дз. TestCase 💼

--- src/test/java/models/Testcase.java
+++ src/test/java/models/Testcase.java
@@ -1,6 +1,21 @@
 package models;
 
-public class Testcase {
+import com.google.gson.annotations.SerializedName;
+import lombok.Builder;
+import lombok.Data;
 
+public class Testcase {
+    @Data
+    @Builder
+    public class TestCase {
+        private String title;
+        private int type;
+
+        @SerializedName(value = "Priority")
+        private int priority;
+
+        @SerializedName(value = "References")
+        private String references;
+    }
 
 }
--- src/test/java/tests/apiHw/TestCaseTest.java
+++ src/test/java/tests/apiHw/TestCaseTest.java
@@ -1,4 +1,32 @@
 package tests.apiHw;
 
-public class TestCaseTest {
+import baseEntities.BaseApiTest;
+import com.google.gson.Gson;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import models.Testcase;
+import org.apache.http.HttpStatus;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.EndpointsForTestCases;
+
+import static io.restassured.RestAssured.given;
+
+public class TestCaseTest extends BaseApiTest {
+    private int testCaseId = 1;
+
+    @Test
+    public void getTestcase() {
+
+        Response response = (Response) given()
+                .pathParam("case_id", testCaseId)
+                .get(EndpointsForTestCases.GET_TESTCASE)
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .as(Testcase.class);
+    }
+
 }
